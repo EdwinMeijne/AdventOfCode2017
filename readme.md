2f6b3b22890f6e06d8cbc325bbc85188b0222d33
Edwin Meijne <git@edwinmeijne.nl>
2017-11-30 16:53:16 +0100

    Advent Warmup: Elvish Cheat Codes

---
+++ readme.md
@@ -0,0 +1,7 @@
+#Advent of Code 2017
+
+Code for the advent of code 2017. Feel free to use or give feedback!
+
+[https://adventofcode.com](https://adventofcode.com)
+
+Written in `TypeScript` and using `ts-node` to run. First `npm install`, then use `package.json` scripts to run each assignment. 
---
+++ src/00/index.ts
@@ -0,0 +1,82 @@
+import {input} from './input';
+
+interface IMarker {
+    x: number,
+    y: number
+}
+
+interface IMarkers {
+    a: IMarker[];
+    b: IMarker[];
+    x: number,
+    y: number,
+}
+
+const inputArray: string[] = input.replace(/ /g, '').split(',');
+
+// First convert input into two arrays of placed markers
+const {a: markersA, b: markersB}: IMarkers = inputArray.reduce(reduceToMarkerArrays, {a: [], b: [], x: 0, y: 0});
+
+// Calculate distance of both A and B markers by finding the largest taxi distance
+const distanceA = markersA.map((marker) => taxiDistance(marker)).reduce(findMax, 0);
+const distanceB = markersB.map((marker) => taxiDistance(marker)).reduce(findMax, 0);
+
+console.log(`'A' markers: ${markersA.length}`);
+console.log(`'B' markers: ${markersB.length}`);
+console.log('Answer 1: ' + Math.max(distanceA, distanceB));
+
+// Calculate distance between all possible pairs and find the max value
+const longestDistance = markersA
+    .map((marker1) => {
+        return markersB.map((marker2) => {
+            return taxiDistance(marker1, marker2);
+        }).reduce(findMax, 0);
+    }).reduce(findMax, 0);
+
+console.log(' ');
+console.log(`Tested ${markersA.length * markersB.length} combinations!`);
+console.log('Answer 2: ' + longestDistance);
+
+// --------------------------------
+
+function reduceToMarkerArrays(markerArrays: IMarkers, item: string) {
+    switch (item) {
+        case 'Down':
+            markerArrays.y--;
+            break;
+        case 'Up':
+            markerArrays.y++;
+            break;
+        case 'Left':
+            markerArrays.x--;
+            break;
+        case 'Right':
+            markerArrays.x++;
+            break;
+        case 'A':
+            markerArrays.a.push({
+                x: markerArrays.x,
+                y: markerArrays.y,
+            });
+            break;
+        case 'B':
+            markerArrays.b.push({
+                x: markerArrays.x,
+                y: markerArrays.y,
+            });
+            break;
+        default:
+    }
+    return markerArrays;
+}
+
+function findMax(furthest: number, taxiDistance: number) {
+    return Math.max(taxiDistance, furthest);
+}
+
+function taxiDistance(marker1, marker2 = {x: 0, y: 0}): number {
+    const distanceX = (marker1.x > marker2.x) ? Math.abs(marker1.x - marker2.x) : Math.abs(marker2.x - marker1.x);
+    const distanceY = (marker1.y > marker2.y) ? Math.abs(marker1.y - marker2.y) : Math.abs(marker2.y - marker1.y);
+    return distanceX + distanceY;
+}
+
