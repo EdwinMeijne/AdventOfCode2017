f2aa8e7aa68f02299cc0117f2f2f8dfc3b85063b
Edwin Meijne <git@edwinmeijne.nl>
2017-12-01 09:26:49 +0100

    * day 1 part 2 added and refactored part 1

--- src/01/index.ts
+++ src/01/index.ts
@@ -3,18 +3,17 @@ console.time('Total time');
 import {input} from './input';
 
 // Extract to array and add first character to the end to simulate a loop
-const inputArray = input.split('').concat([input.charAt(0)]).map((num) => parseInt(num));
+const inputArray = input.split('').map((num) => parseInt(num));
 
-interface IAcc {
-    last: number,
-    sum: number,
-}
+const distance = Math.floor(inputArray.length / 2);
+let sum1 = 0;
+let sum2 = 0;
 
-const {sum} = inputArray.reduce((acc: IAcc, item: number) => {
-    if (acc.last === item) acc.sum += item;
-    acc.last = item;
-    return acc;
-}, { last: 0, sum: 0});
+inputArray.forEach((value, index, array) => {
+    if (value === array[(index + 1) % array.length]) sum1 += value;
+    if (value === array[(index + distance) % array.length]) sum2 += value;
+});
 
-console.log('Answer 1: ' + sum);
+console.log('Answer 1: ' + sum1);
+console.log('Answer 2: ' + sum2);
 console.timeEnd('Total time');
