0e8179567f635957688fce3a0231e9060f04caf6
Edwin Meijne <git@edwinmeijne.nl>
2017-12-04 22:01:31 +0100

    * day 4, both answers

--- src/04/index.ts
+++ src/04/index.ts
@@ -1,8 +1,25 @@
 console.time('Total time');
 
 import {input} from './input';
+const phrases: string[] = input.split('\n');
 
-console.log('Answer 1: ');
-console.log('Answer 2: ');
+console.log('Answer 1: ' + getNonDuplicatingPhrases(phrases).length);
+console.log('Answer 2: ' + getNonAnagramPhrases(phrases).length);
 
 console.timeEnd('Total time');
+
+// --------------------------------
+
+function getNonDuplicatingPhrases(phrases: string[]) {
+    return phrases.filter((phrase) => {
+        const phraseArr = phrase.split(' ');
+        return (new Set(phraseArr)).size === phraseArr.length;
+    });
+}
+
+function getNonAnagramPhrases(phrases: string[]) {
+    return phrases.filter((phrase) => {
+        const phraseArrSorted = phrase.split(' ').map(word => word.split('').sort().toString());
+        return (new Set(phraseArrSorted)).size === phraseArrSorted.length;
+    });
+}
