edca70ab54c1a39abca5df96d341d03a6d7b4144
Edwin Meijne <git@edwinmeijne.nl>
2017-12-04 21:16:59 +0100

    * day 3 part 2!

--- src/03/index.ts
+++ src/03/index.ts
@@ -4,6 +4,7 @@ import {input} from './input';
 const memLoc = parseInt(input);
 
 console.log('Answer 1: ' + taxiDistanceToInputMemory(memLoc));
+console.log('Answer 2: ' + stressTest(memLoc));
 
 console.timeEnd('Total time');
 
@@ -45,6 +46,59 @@ function taxiDistanceToInputMemory(input) {
     }
 
     return taxiDistance(zeroPoint, memPoint);
+}
+
+function stressTest(memLoc: number) {
+    const enum directions {
+        'right',
+        'up',
+        'left',
+        'down',
+    }
+
+    const gridValues = [{
+        x: 0,
+        y: 0,
+        value: 1,
+    }];
+
+    let x = 0;
+    let y = 0;
+    let direction = directions.right;
+    let value = 1;
+
+    do {
+        // Calculate new position of x / y and new direction based on 'if we can go around the corner yet'
+        switch (direction) {
+            case directions.right:
+                x++;
+                direction = (gridValues.some((entry) => entry.y === y - 1 && entry.x === x)) ? direction : directions.up;
+                break;
+            case directions.up:
+                y--;
+                direction = (gridValues.some((entry) => entry.y === y && entry.x === x - 1)) ? direction : directions.left;
+                break;
+            case directions.left:
+                x--;
+                direction = (gridValues.some((entry) => entry.y === y + 1 && entry.x === x)) ? direction : directions.down;
+                break;
+            case directions.down:
+                y++;
+                direction = (gridValues.some((entry) => entry.y === y && entry.x === x + 1)) ? direction : directions.right;
+                break;
+        }
+
+        value = gridValues
+            .filter((entry) => Math.abs(entry.x - x) <= 1 && Math.abs(entry.y - y) <= 1) // grab all fields around item
+            .reduce((total, {value}) => total + value, 0); // count total
+
+        gridValues.push({x, y, value});
+
+    } while (value < memLoc);
+
+    console.log(gridValues.map(val => ({x:val.x, y:val.y})));
+
+    return value;
 }
 
 function calculateSize(input: number) {
