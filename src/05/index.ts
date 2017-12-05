44344ad337b35aadd189800ce63d12cf0911b75b
Edwin Meijne <git@edwinmeijne.nl>
2017-12-05 07:44:38 +0100

    * day 5!

---
+++ src/05/index.ts
@@ -0,0 +1,35 @@
+console.time('Total time');
+
+import {input} from './input';
+const jumps: number[] = input.split('\n').map((val) => parseInt(val));
+
+console.log('Answer 1: ' + breakout([...jumps]));
+console.log('Answer 2: ' + breakoutDifferent([...jumps]));
+
+console.timeEnd('Total time');
+
+// --------------------------------
+
+function breakout(jumpArr: number[]) {
+    let index = 0;
+    let steps = 0;
+
+    do {
+        steps++;
+        index += jumpArr[index]++;
+    } while(jumpArr.length > index);
+
+    return steps;
+}
+
+function breakoutDifferent(jumpArr: number[]) {
+    let index = 0;
+    let steps = 0;
+
+    do {
+        steps++;
+        index += (jumpArr[index] >= 3) ? jumpArr[index]-- : jumpArr[index]++;
+    } while(jumpArr.length > index || index < 0);
+
+    return steps;
+}
