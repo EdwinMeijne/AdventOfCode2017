659d308f8d681d8558a336aa7e03f1bf5b3e8f24
Edwin Meijne <git@edwinmeijne.nl>
2017-12-08 09:47:29 +0100

    * day 8 added

---
+++ src/08/index.ts
@@ -0,0 +1,11 @@
+import {input} from './input';
+
+console.time('Total time');
+
+console.log('Answer 1: ');
+console.log('Answer 2: ');
+
+console.timeEnd('Total time');
+
+// --------------------------------
+
